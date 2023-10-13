06626ef48d56fb40baa3a214b759af637f257a3d
tyn <tuyenpham1104.dev@gmail.com>
2023-10-14 00:07:50 +0700

    make status campaigns

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,3 +9,31 @@ export const isAxiosErrorUnprocessableEntity = <T>(error: unknown): error is Axi
 }
 
 export type Coordinate = { lng?: string; lat?: string; name?: string }
+
+export const campaignStatus = (start: string, end: string) => {
+  const now = new Date()
+  const start_date = new Date(start)
+  const end_date = new Date(end)
+  if (now < start_date) {
+    return 'Chưa diễn ra'
+  } else {
+    if (now <= end_date) {
+      return 'Đang diễn ra'
+    } else {
+      return 'Đã kết thúc'
+    }
+  }
+}
+
+export const statusColor = (start: string, end: string) => {
+  const status = campaignStatus(start, end)
+  if (status === 'Chưa diễn ra') {
+    return 'text-[#747474]'
+  }
+  if (status === 'Đang diễn ra') {
+    return 'text-green-500'
+  }
+  if (status === 'Đã kết thúc') {
+    return 'text-red-500'
+  }
+}
